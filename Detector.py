0957b205da7764e6780988411508e03bc18b768d
sStunOne <38848467+sStunOne@users.noreply.github.com>
2018-11-02 23:34:05 -0500

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -3,10 +3,15 @@ import numpy as np
 
 class Deteccion:
     def __init__(self,ruta):
-        self.mser = opencv.MSER_create ()
-        self.imagen = opencv.imread (ruta)
+        self.mser = opencv.MSER_create()
+        self.imagen = opencv.imread(ruta)
     def impresion(self):
         print(self.imagen)
+    def conversion(self):
+        transfor = opencv.cvtColor (self.imagen, opencv.COLOR_BGR2GRAY)
+        print(transfor)
+        ejecucion = self.mser.detectRegions(transfor)
+        print(ejecucion[0][1])
 
 intento = Deteccion('C:\images.png')
-intento.impresion()
+intento.conversion()
